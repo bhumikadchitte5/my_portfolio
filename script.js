2e126af5d4bd498f1db8f884fcd3923d2162c105
bhumikadchitte5 <bhumikadchitte5@gmail.com>
2025-02-15 22:45:39 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//     for (let i = 0; i < 50; i++) {
+//         let dot = document.createElement("div");
+//         dot.classList.add("dot");
+//         dot.style.left = Math.random() * window.innerWidth + "px";
+//         dot.style.top = Math.random() * window.innerHeight + "px";
+//         dot.style.animationDuration = (Math.random() * 2 + 2) + "s";
+//         document.body.appendChild(dot);
+//     }
+// });
+
